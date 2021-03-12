5e070babc8a5969b4447404b08a5bb71956eddee
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-12 20:25:20 +0100

    Esercizio migliorato con le funzioni!

--- main.js
+++ main.js
@@ -1,36 +1,62 @@
 $(document).ready(function() {
+  mouseEnter();
+  mouseLeave();
+});
+
+
+
+function mouseEnter() {
+
   var btnDropdown = $('form.d-flex ul li.dropdown a.nav-link');
   var dropdownMenu = $('form ul li ul.dropdown-menu');
 
-
   btnDropdown.mouseenter(function() {
-
     if (!($(this).hasClass('show'))) {
       $(this).addClass('show');
       $(this).attr('aria-expanded', 'false');
       $(this).next().addClass('show');
       $(this).next().attr('data-bs-propper', 'none');
     };
-
   });
 
+  dropdownMenu.mouseenter(function() {
+    if (!($(this).hasClass('show'))) {
+      $(this).addClass('show');
+      $(this).attr('aria-expanded', 'false');
+      $(this).prev().addClass('show');
+      $(this).prev().attr('data-bs-propper', 'none');
+    };
+  });
+
+};
+
+
+
+
+function mouseLeave() {
+  var btnDropdown = $('form.d-flex ul li.dropdown a.nav-link');
+  var dropdownMenu = $('form ul li ul.dropdown-menu');
 
   btnDropdown.mouseleave(function() {
-
     if ($(this).hasClass('show')) {
       $(this).removeClass('show');
-      $(this).attr('aria-expanded', 'true');
+      $(this).attr('aria-expanded', 'false');
       $(this).next().removeClass('show');
       $(this).next().removeAttr('data-bs-propper');
     };
-
   });
 
+  dropdownMenu.mouseleave(function() {
+    if ($(this).hasClass('show')) {
+      $(this).removeClass('show');
+      $(this).attr('aria-expanded', 'false');
+      $(this).prev().removeClass('show');
+      $(this).prev().removeAttr('data-bs-propper');
+    };
+  });
+};
 
-  // btnDropdown  show  aria-expanded:"true" / show scompare,  "false"
-  // dropdownMenu   show data-bs-propper:"none"; / tutti due scompaiano;
 
 
-
-
-});
+// btnDropdown  show  aria-expanded:"true" / show scompare,  "false"
+// dropdownMenu   show data-bs-propper:"none"; / tutti due scompaiano;
