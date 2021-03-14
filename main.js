b97ca75bfbbcf11a8637b62e9c9f977891744e28
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-14 18:21:38 +0100

    Progetto finito!

--- main.js
+++ main.js
@@ -3,107 +3,23 @@ $(document).ready(function() {
   mouseLeave();
 });
 
-
-
 function mouseEnter() {
+  var enter = $('.enter');
+  var myCard = $('.row');
 
-  var btnDropdown = $('form.d-flex ul li.dropdown a.nav-link');
-  var dropdownMenu = $('form .dropdown-menu');
-  var myDropdown = $('form.d-flex .my-dropdown');
-  var myFeatures  = $('form.d-flex li.my-features');
-  var bothMy = $('.both-leave-enter');
-
- bothMy.mouseenter(function() {
-    if(!($(this).hasClass('active'))){
-      $(this).children('.my-dropdown').addClass('active')
-    }
- });
-
-  btnDropdown.mouseenter(function() {
-    if (!($(this).hasClass('show'))) {
-      $(this).addClass('show');
-      $(this).attr('aria-expanded', 'false');
-      $(this).next().addClass('show');
-      $(this).next().attr('data-bs-propper', 'none');
-    };
-  });
-
-  dropdownMenu.mouseenter(function() {
-    if (!($(this).hasClass('show'))) {
-      $(this).attr('aria-expanded', 'false');
-      $(this).prev().addClass('show');
-      $(this).prev().attr('data-bs-propper', 'none');
-    };
-  });
-
-  myFeatures.mouseenter(function() {
-     if(!($(this).hasClass('active'))){
-       $(this).children('.my-dropdown').addClass('active')
-     }
-  });
+    enter.mouseenter(function() {
+      $(this).find('.row').addClass('active');
+    });
 
 
-  myDropdown.mouseenter(function(){
-    if(!($(this).hasClass('active'))){
-      myDropdown.addClass('active');
-    }
-  });
-
-};
-
-
+}
 
 
 function mouseLeave() {
-  var btnDropdown = $('form.d-flex ul li.dropdown a.nav-link');
-  var dropdownMenu = $('form .dropdown-menu');
-  var myDropdown = $('form.d-flex .my-dropdown');
-  var myFeatures  = $('form.d-flex li.my-features');
-  var bothMy = $('.both-leave-enter');
+  var myCard = $('.row');
 
+  myCard.mouseleave(function() {
+    myCard.removeClass('active');
 
-
-  btnDropdown.mouseleave(function() {
-    if ($(this).hasClass('show')) {
-      $(this).removeClass('show');
-      $(this).attr('aria-expanded', 'false');
-      $(this).next().removeClass('show');
-      $(this).next().removeAttr('data-bs-propper');
-    };
   });
-
-  dropdownMenu.mouseleave(function() {
-    if ($(this).hasClass('show')) {
-      $(this).removeClass('show');
-      $(this).attr('aria-expanded', 'false');
-      $(this).prev().removeClass('show');
-      $(this).prev().removeAttr('data-bs-propper');
-    };
-  });
-
-  bothMy.mouseleave(function() {
-     if($(this).children('.my-dropdown').hasClass('active')){
-       $(this).children('.my-dropdown').removeClass('active')
-     }
-  });
-
-  myFeatures.mouseleave(function() {
-     if($(this).children('.my-dropdown').hasClass('active')){
-       $(this).children('.my-dropdown').removeClass('active')
-     }
-  });
-
-myDropdown.mouseleave(function(){
-  if($(this).hasClass('active')){
-    myDropdown.removeClass('active');
-  }
-});
-
 };
-
-
-
-
-
-// btnDropdown  show  aria-expanded:"true" / show scompare,  "false"
-// dropdownMenu   show data-bs-propper:"none"; / tutti due scompaiano;
